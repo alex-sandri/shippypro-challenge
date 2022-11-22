06cc80076099f25d6a1d52f7ab4b10835cdc2216
Alex Sandri <me@alexsandri.com>
2022-11-22 10:46:18 +0100

    add code for connecting to the db, fix sorting order and use `usort` as `uasort` maintains index associations

--- backend/index.php
+++ backend/index.php
@@ -6,19 +6,29 @@ require_once './src/trips.php';
 $from = $_GET['from'] ?? '';
 $to = $_GET['to'] ?? '';
 
+$connection = pg_connect('postgresql://postgres:password@db:5432/shippypro');
+
+if (!$connection) {
+  exit('could not connect to the database');
+}
+
 if (empty($from) && empty($to)) {
-  $result = list_airports();
+  $result = list_airports($connection);
 } else {
-  $trips = find_trips(from: $from, to: $to);
+  $trips = find_trips($connection, from: $from, to: $to);
 
   if (empty($trips)) {
     $result = [];
   } else {
-    uasort($trips, fn(Trip $a, Trip $b) => $b->price() - $a->price());
+    // Sort trips by price in ascending order so that the first trip
+    // in the array is the cheapest one.
+    usort($trips, fn(Trip $a, Trip $b) => $a->price() - $b->price());
 
     $result = [$trips[0]];
   }
 }
+
+pg_close($connection);
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
