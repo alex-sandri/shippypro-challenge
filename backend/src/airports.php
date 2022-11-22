47a13d8bd1bfe729d95229c6c6d78ba18c19782b
Alex Sandri <me@alexsandri.com>
2022-11-22 10:46:33 +0100

    retrieve airports from db

--- backend/src/airports.php
+++ backend/src/airports.php
@@ -22,63 +22,26 @@ class Airport {
   }
 }
 
-function list_airports(): array {
-  return [
-    new Airport(
-      id: 0,
-      name: 'Milano Malpensa',
-      code: 'MXP',
-      lat: 45.630606,
-      lng: 8.728111,
-    ),
-    new Airport(
-      id: 1,
-      name: 'Venezia Marco Polo',
-      code: 'VCE',
-      lat: 45.505278,
-      lng: 12.351944,
-    ),
-    new Airport(
-      id: 2,
-      name: 'Los Angeles International Airport',
-      code: 'LAX',
-      lat: 33.942536,
-      lng: -118.408075,
-    ),
-    new Airport(
-      id: 3,
-      name: 'John F. Kennedy International Airport',
-      code: 'JFK',
-      lat: 40.639751,
-      lng: -73.778925,
-    ),
-    new Airport(
-      id: 4,
-      name: "O'Hare International Airport",
-      code: 'ORD',
-      lat: 41.978603,
-      lng: -87.904842,
-    ),
-    new Airport(
-      id: 5,
-      name: 'Frankfurt Airport',
-      code: 'FRA',
-      lat: 50.026421,
-      lng: 8.543125,
-    ),
-    new Airport(
-      id: 6,
-      name: 'Paris Charles de Gaulle Airport',
-      code: 'CDG',
-      lat: 49.012779,
-      lng: 2.548925,
-    ),
-    new Airport(
-      id: 7,
-      name: 'Singapore Changi Airport',
-      code: 'SIN',
-      lat: 1.350189,
-      lng: 103.994433,
-    ),
-  ];
+function list_airports($connection): array {
+  $result = pg_query($connection, 'select * from "airport"');
+
+  if (!$result) {
+    exit('could not retrieve airports');
+  }
+
+  $airports = [];
+
+  while ($row = pg_fetch_assoc($result)) {
+    $airports[] = new Airport(
+      id: $row['id'],
+      name: $row['name'],
+      code: $row['code'],
+      lat: $row['lat'],
+      lng: $row['lng'],
+    );
+  }
+
+  pg_free_result($result);
+
+  return $airports;
 }
